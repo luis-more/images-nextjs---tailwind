f4b01507664689bb9b03658ab190d0e12b07cede
luis-more <mmore3369@gmail.com>
2022-07-22 11:58:28 -0500

    actualizando el navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <Link>
+          <h1>LOGO</h1>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
